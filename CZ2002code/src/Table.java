db3a3dc0e5a414518e3237159a69a917f3fa2a9b
Sahaj <86873928+sgx00@users.noreply.github.com>
2021-10-27 17:02:51 +0800

    Create Table.java

---
+++ CZ2002code/src/Table.java
@@ -0,0 +1,43 @@
+package resSys;
+
+public class Table {
+	private int number;
+	private int seating;
+	private boolean reserved;
+	private boolean occupied ;
+	
+	public Table(int number, int seating, boolean reserved, boolean occupied)
+	{
+		this.number = number;
+		this.seating = seating;
+		this.reserved = reserved;
+		this.occupied = occupied;
+	}
+	
+	public void makeReservation(boolean res)
+	{
+		reserved = res;
+	}
+	
+	public void setOccupied(boolean occupied)
+	{
+		this.occupied= occupied;
+	}
+	
+	public boolean isReserved()
+	{
+		return reserved;
+	}
+	
+	public boolean isOccupied()
+	{
+		return occupied;
+	}
+	
+	public int getTableNo()
+	{
+		return number;
+	}
+	
+
+}
