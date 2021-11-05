78d9b0debfac55b658bec95af60047c4eabdd513
TANXINKAI <81215661+TANXINKAI@users.noreply.github.com>
2021-11-05 20:48:39 +0800

    Display based on food type

--- CZ2002code/src/menu/MenuDisplay.java
+++ CZ2002code/src/menu/MenuDisplay.java
@@ -6,15 +6,57 @@ public class MenuDisplay {
 		int menuItemArraySize = menu.getMenuItemArray().size();
 		System.out.println("List of Menu Item");
 		for(int i = 0; i < menuItemArraySize; i++) {
-			//System.out.println(i+1 + ". " + menu.getMenuItemArray().get(i).getFoodName() + "\t--S$" + menu.getMenuItemArray().get(i).getFoodPrice());
 			System.out.format("%-10d%-50s%16.2f\n",i+1, menu.getMenuItemArray().get(i).getFoodName(), menu.getMenuItemArray().get(i).getFoodPrice());
-	
+		}
+	}
+
+	public static void menuItemPrintdescription(Menu menu) {
+		int menuItemArraySize = menu.getMenuItemArray().size();
+		System.out.println("List of Menu Item");
+		for(int i = 0; i < menuItemArraySize; i++) {
+			System.out.format("%-10d%-50s%16.2f\n",i+1, menu.getMenuItemArray().get(i).getFoodName(), menu.getMenuItemArray().get(i).getFoodPrice());
+			System.out.format("          %-50s\n", menu.getMenuItemArray().get(i).getFoodDescription());
+		}
+	}
+
+	public static void TypemenuItemPrintdescription(Menu menu) {
+		int menuItemArraySize = menu.getMenuItemArray().size();
+		System.out.println("List of Menu Item");
+		String foodtypearray[] = {"Mains", "Sides", "Drinks", "Desserts"};
+		
+		for(String s : foodtypearray ){
+			System.out.format("---------------------------------- %-10s ----------------------------------\n", s);
+			for(int i = 0; i < menuItemArraySize; i++) {
+				
+				if(s.equals(menu.getMenuItemArray().get(i).getFoodType())){
+					System.out.format("%-10d%-50s%16.2f\n",i+1, menu.getMenuItemArray().get(i).getFoodName(), menu.getMenuItemArray().get(i).getFoodPrice());
+					System.out.format("          :  %-50s\n", menu.getMenuItemArray().get(i).getFoodDescription());
+					System.out.println();
+				}
+				
 		}
 	}
 	
 	public static void promotionPrintName(Menu menu) {
+		int menuItemArraySize = menu.getMenuItemArray().size();
 		int promotionArraySize = menu.getPromotionArray().size();
 		System.out.println("List of Promotion");
+		boolean exists = false;
+		
+		/*
+		for(int i = 0; i < promotionArraySize; i++) {
+			for(int j = 0; j < menuItemArraySize; j++) {
+				println(menu.getPromotionArray().get(i).getFoodArray());
+				break;
+			}
+			if(exists == false){
+				System.out.format("%-10d%-50s%16.2f\n",i+1, menu.getPromotionArray().get(i).getPromotionName(), menu.getPromotionArray().get(i).getPromotionPrice());	
+			}
+			*/
+
+	
+
+
 		for(int i = 0; i < promotionArraySize; i++) {
 			//System.out.println(i+1 + ". " + menu.getPromotionArray().get(i).getPromotionName() + "\t--S$" + menu.getPromotionArray().get(i).getPromotionPrice());
 			System.out.format("%-10d%-50s%16.2f\n",i+1, menu.getPromotionArray().get(i).getPromotionName(), menu.getPromotionArray().get(i).getPromotionPrice());		
@@ -25,5 +67,9 @@ public class MenuDisplay {
 		menuItemPrintName(menu);
 		System.out.println();
 		promotionPrintName(menu);
+		System.out.println();
+		menuItemPrintdescription(menu);
+		System.out.println();
+		TypemenuItemPrintdescription(menu);
 	}
 }
