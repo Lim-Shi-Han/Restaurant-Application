5b55cc8f9307d03cb03dfc20d9a7604c7a84122a
AnantG <62785559+Creator117@users.noreply.github.com>
2021-11-09 10:02:48 +0800

    Update Calendar.java

--- CZ2002code/src/application/Calendar.java
+++ CZ2002code/src/application/Calendar.java
@@ -1,40 +1,47 @@
-// package application;
-
-// import java.util.*;  
-// public class Calendar{
-  
-// 	  Calendar calendar = Calendar.getInstance();
-// 	  private String timeStamp;
-// 	  private int week;
-// 	  private int month;
-// 	  private int day;
-    
-// 	  public String timeStamp(){
-// 		  timeStamp = calendar.getTime());
-// 		  return timeStamp;
-// 	  }
-  
-// 	  public int getWeek(){
-// 		  week = calendar.getWeekYear;
-// 		  return week;
-// 	  }
-  
-// 	  public int getMonth(){
-// 		  month = calendar.get(Calendar.MONTH);// NOTE!!! : Month from 0 to 11
-// 		  return month;
-// 	  }
-  
-// 	  public int getDay(){
-// 		  day = calendar.get(Calendar.DAY_OF_MONTH);
-// 		  return day;
-// 	  }
-// }
 package application;
-import java.sql.Timestamp;
-public class Calendar {
-   public static void main(String[] args) {
-     Long datetime = System.currentTimeMillis();
-       Timestamp timestamp = new Timestamp(datetime);
-       System.out.println("Current Time Stamp: "+timestamp);
-   }
+
+import java.util.*;  
+public class Calendar{
+  
+  Calendar calendar = Calendar.getInstance();
+  Date reserved = calendar.getTime();
+  private String timeStamp;
+  private int week;
+  private int month;
+  private int day;
+    
+  public String timeStamp(){
+    timeStamp = calendar.getTime());
+    return timeStamp;
+  }
+  
+  public int getWeek(){
+    week = calendar.getWeekYear;
+    return week;
+  }
+  
+  public int getMonth(){
+    month = calendar.get(Calendar.MONTH);// NOTE!!! : Month from 0 to 11
+    return month;
+  }
+  
+  public int getDay(){
+    day = calendar.get(Calendar.DAY_OF_MONTH);
+    return day;
+  }
+  
+  public reservedDate(int Year, int Month, int Day, int Hour, int Min){
+    Calendar rDate = new GregorianCalendar(Year, Month, Day, Hour, Min)
+  }
+  
 }
+
+//package application;
+//import java.sql.Timestamp;
+//public class Calendar {
+//   public static void main(String[] args) {
+//     Long datetime = System.currentTimeMillis();
+//       Timestamp timestamp = new Timestamp(datetime);
+//       System.out.println("Current Time Stamp: "+timestamp);
+//   }
+//}
