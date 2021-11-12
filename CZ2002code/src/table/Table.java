b1a43477f57a7805a7224a70395a526c893601d0
GodForsake <dhruv010@e.ntu.edu.sg>
2021-11-12 20:54:00 +0800

    table

--- CZ2002code/src/table/Table.java
+++ CZ2002code/src/table/Table.java
@@ -1,48 +1,22 @@
 package table;
 
 public class Table {
-	private int number;
-	private int seating;
-	private boolean reserved;
-	private boolean occupied;
-        private int nump;
-	
-	public Table(int number, int seating, boolean reserved, boolean occupied)
-	{
-		this.number = number;
-		this.seating = seating;
-		this.reserved = reserved;
-		this.occupied = occupied;
-	}
-	
-	public void makeReservation(boolean res)
-	{
-		reserved = res;
-	}
-	public void addppl(int noperson)
-	{
-		nump = noperson;
-	}
-	
-	public void setOccupied(boolean occupied)
-	{
-		this.occupied= occupied;
-	}
-	
-	public boolean isReserved()
-	{
-		return reserved;
-	}
-	
-	public boolean isOccupied()
-	{
-		return occupied;
-	}
-	
-	public int getTableNo()
-	{
-		return number;
-	}
-	
+		private int number;
+		private int seating;
+		
+		public Table(int number, int seating)
+		{
+			this.number = number;
+			this.seating = seating;
+		}
 
+		public int numberSeats()
+		{
+			return seating;
+		}
+		
+		public int getTableNo()
+		{
+			return number;
+		}
 }
