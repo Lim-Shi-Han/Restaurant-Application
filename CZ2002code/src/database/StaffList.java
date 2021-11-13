fdbe2107e4e155816bc579f0cbf458ddaedc68f3
Lim-Shi-Han <79437795+Lim-Shi-Han@users.noreply.github.com>
2021-11-13 16:20:59 +0800

    Update StaffList.java

--- CZ2002code/src/database/StaffList.java
+++ CZ2002code/src/database/StaffList.java
@@ -2,6 +2,7 @@ package database;
 import entity.Staff;
 import java.io.Serializable;
 import java.util.*;
+import manager.*;
 
 public class StaffList implements Serializable{
 
@@ -15,6 +16,11 @@ public class StaffList implements Serializable{
         Staff s3 = new Staff("Charlie", "Male", 89675, "Waiter");
         Staff s4 = new Staff("Donald", "Male", 46564, "Chef");
         Staff s5 = new Staff("Eric", "Male", 34212, "Manager");
+        staffArray.add(s1);
+        staffArray.add(s2);
+        staffArray.add(s3);
+        staffArray.add(s4);
+        staffArray.add(s5); 
     }
 
 
@@ -24,6 +30,11 @@ public class StaffList implements Serializable{
 
     public void setStaffArray(ArrayList<Staff> staffArray) {
         this.staffArray = staffArray;
+    }
+
+    public static void main(String[] args){
+        StaffList sl = new StaffList();
+        
     }
 }
 
