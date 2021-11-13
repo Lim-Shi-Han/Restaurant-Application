1fb7a2e7a0ebd1904196b5af6039d62e1ce5b6e7
TANXINKAI <81215661+TANXINKAI@users.noreply.github.com>
2021-11-13 19:26:27 +0800

    constants interface

---
+++ CZ2002code/src/entity/Constants.java
@@ -0,0 +1,8 @@
+package entity;
+
+public interface Constants {
+    public static final String Currency = "SGD";
+    public static final double GST = 0.07;
+    public static final double service_charge = 0.1;
+    public static final double member_discount = 0.1;
+}
